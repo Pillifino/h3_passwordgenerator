4ce1aba9d34c81002dc49e7864744c937d1c5503
Pillifino <dinodizon073100@gmail.com>
2022-03-05 22:45:36 -0800

    Added array to write password function that shows password criteria

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+	var generatePassword = ["Must Have Uppercase", "Must Have Lower Case", "Must Have Symbol","Must Have Number"]
+	// For Loop With Password Criteria
+	for (var i = 0; i < generatePassword.length; i++)
+		console.log(generatePassword[i]);
+	// For Loop With Password Criteria
+	var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
